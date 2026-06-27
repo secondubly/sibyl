74d35914ddaa06e34b61a4ccba7396a5a5cdf0c5
secondubly <secondubly@gmail.com>
2026-06-27 12:25:27 -0400

    set up mantine

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { MantineProvider } from '@mantine/core'
+
+// import '@mantine/core/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App /> 
+    </MantineProvider>
   </StrictMode>,
 )
